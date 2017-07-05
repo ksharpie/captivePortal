7f151260681042087793584e6e4db4e6a78d4ecd
Khalid Sharpe <sharpe.khalid@gmail.com>
2017-07-05 15:54:31 -0500

    adding code for captive portal

--- resources/views/captivePortal/landing.blade.php
+++ resources/views/captivePortal/landing.blade.php
@@ -99,3 +99,17 @@
   	}
   }
 </style>
+<script>
+  $(".carousel").swipe({
+
+    swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+
+      if (direction == 'left') $(this).carousel('next');
+      if (direction == 'right') $(this).carousel('prev');
+
+
+    },
+    allowPageScroll:"vertical"
+
+  });
+</script>
