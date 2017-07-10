9a9618e6ae15b24a5534f92b5daa4aefe4abc5db
Khalid Sharpe <sharpe.khalid@gmail.com>
2017-07-10 16:05:49 -0500

    adjusting video to work properly on mobile devices

--- resources/views/captivePortal/landing.blade.php
+++ resources/views/captivePortal/landing.blade.php
@@ -47,7 +47,7 @@
               <img src="{{ $advertisements[$count]->logo_path }}" alt="{{ $advertisements[$count]->company_name }}">
           @else
             @if ($count == $initialAdvertisement)
-              <video autoplay loop>
+              <video playsinline autoplay loop muted>
             @else
               <video>
             @endif
@@ -207,7 +207,7 @@
           $(this).replaceWith( "<video>" + $(this).html() + "</video>" );
         });
 
-        $('.item.active').find('video').replaceWith( "<video autoplay loop>" + $('.item.active').find('video').html() + "</video>" );
+        $('.item.active').find('video').replaceWith( "<video playsinline autoplay loop muted>" + $('.item.active').find('video').html() + "</video>" );
       }
 
       setTimeout(delayedResponse,605);
